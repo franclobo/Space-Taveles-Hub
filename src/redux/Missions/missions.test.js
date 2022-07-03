b86d597b9433bc27c65f0d0d984712941d4d9239
franclobo <fjbl2788@gmail.com>
2022-07-02 21:04:22 -0500

    Create test for missions.

---
+++ src/redux/Missions/missions.test.js
@@ -0,0 +1,43 @@
+import missionsReducer, { joinMission, loadMissions, leaveMission } from "./missions";
+
+describe("missionsReducer test", () => {
+  test ("initial state", () => {
+    expect(missionsReducer(undefined, {})).toEqual([]);
+  }
+  );
+  test ("joinMission", () => {
+    expect(missionsReducer([{
+      id: "M1",
+      name: "Mission 1",
+      description: "Description 1",
+      joined: false,
+    }], {
+      type: "JOIN_MISSION",
+      payload: "M1",
+    })).toEqual([{
+      id: "M1",
+      name: "Mission 1",
+      description: "Description 1",
+      joined: true,
+    }]);
+  }
+  );
+  test ("leaveMission", () => {
+    expect(missionsReducer([{
+      id: "M1",
+      name: "Mission 1",
+      description: "Description 1",
+      joined: true,
+    }], {
+      type: "LEAVE_MISSION",
+      payload: "M1",
+    })).toEqual([{
+      id: "M1",
+      name: "Mission 1",
+      description: "Description 1",
+      joined: false,
+    }]);
+  }
+  );
+}
+);
